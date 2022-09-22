e852266e2991614f7b7df73b0e58d8e28ba7789b
TedJenkler <Tedjenkler@gmail.com>
2022-09-22 20:36:20 +0200

    Added popup site

---
+++ js lvl 1 rating popup/thx.js
@@ -0,0 +1,8 @@
+// How to import in vanila js?
+
+let vote = localStorage.getItem("hasVoted")
+let rating = document.querySelector('#rating')
+
+rating.innerHTML = `You selected ${vote} out of 5`
+
+console.log(vote)
---
+++ popup-rating-site.js
@@ -0,0 +1,10 @@
+let btnOne = document.querySelector('#popup-btn-1')
+let btnTwo = document.querySelector('#popup-btn-2')
+
+btnOne.addEventListener('click', (e) => {
+    location.href = './js lvl 1 rating popup/index.html'
+})
+
+btnTwo.addEventListener('click', (e) => {
+    location.href = 'https://github.com/TedJenkler/rating-popup'
+})
